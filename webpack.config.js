bb50cab787616cbb1a25a98c73e45877bf349e59
test@leoni.land <nwsf8f3z>
2021-09-25 13:32:56 +0300

    add isProd and isDev

--- webpack.config.js
+++ webpack.config.js
@@ -2,48 +2,53 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    entry: {
-        main: ['@babel/polyfill','./index.js']
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-            inject: 'body'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].bundle.css'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    MiniCssExtractPlugin.loader,
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ['@babel/preset-env']
+module.exports = (env, argv) => {
+    const isProd = argv.mode === 'production'
+    const isDev = !isProd
+
+    return {
+        context: path.resolve(__dirname, 'src'),
+        entry: {
+            main: ['@babel/polyfill','./index.js']
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].bundle.js'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './index.html',
+                inject: 'body'
+            }),
+            new MiniCssExtractPlugin({
+                filename: '[name].bundle.css'
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        MiniCssExtractPlugin.loader,
+                        // Translates CSS into CommonJS
+                        "css-loader",
+                        // Compiles Sass to CSS
+                        "sass-loader",
+                    ],
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
                     }
                 }
-            }
-        ],
-    }
+            ],
+        }
 
+    }
 }
