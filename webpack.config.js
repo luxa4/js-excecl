7991bbf9b6ddbb9c81270514650c3e032605f94d
test@leoni.land <nwsf8f3z>
2021-09-25 14:27:57 +0300

    add ESLint and Eslint rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,63 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = (env, argv) => {
-    const isProd = argv.mode === 'production'
-    const isDev = !isProd
+  const isProd = argv.mode === 'production'
+  const isDev = !isProd
 
-    return {
-        context: path.resolve(__dirname, 'src'),
-        entry: {
-            main: ['@babel/polyfill','./index.js']
+  const plugins = () => {
+    const base = [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        inject: 'body'
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].bundle.css'
+      }),
+    ]
+
+    if (isDev) base.push(new ESLintPlugin())
+
+    return base
+  }
+
+  return {
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      main: ['@babel/polyfill', './index.js']
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].bundle.js'
+    },
+    plugins: plugins(),
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
         },
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: '[name].bundle.js'
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './index.html',
-                inject: 'body'
-            }),
-            new MiniCssExtractPlugin({
-                filename: '[name].bundle.css'
-            })
-        ],
-        module: {
-            rules: [
-                {
-                    test: /\.s[ac]ss$/i,
-                    use: [
-                        // Creates `style` nodes from JS strings
-                        MiniCssExtractPlugin.loader,
-                        // Translates CSS into CommonJS
-                        "css-loader",
-                        // Compiles Sass to CSS
-                        "sass-loader",
-                    ],
-                },
-                {
-                    test: /\.m?js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ['@babel/preset-env']
-                        }
-                    }
-                }
-            ],
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
         }
-
+      ],
     }
+
+  }
 }
