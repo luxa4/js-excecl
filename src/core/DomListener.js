9c1cdd1928c9bc86f4a13e927291f5ab54ee7e73
Alex Belyaev <test2@leoni.land>
2021-10-19 10:14:38 +0300

    5 lesson

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,9 @@
 export class DomListener {
+  constructor($root) {
+    if (!$root) {
+      throw new Error('$root dont provide for DomListener');
+    }
 
+    this.$root = $root
+  }
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import {Excel} from '@/components/excel/Excel';
 import {Header} from '@/components/header/Header';
 import {Table} from '@/components/table/Table';
 import {Toolbar} from '@/components/toolbar/Toolbar';
-import {Formula} from '@/components/Formula/Formula';
+import {Formula} from '@/components/formula/Formula';
 
 const excel = new Excel('#app', {
-  components: [Header, Table, Toolbar, Formula]
+  components: [Header, Toolbar, Formula, Table]
 });
 
 console.log(excel)
