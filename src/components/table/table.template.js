06357162251074760d6f780e24e487ada5284bb7
Alex Belyaev <test2@leoni.land>
2021-11-29 20:51:36 +0300

    add verstka to header

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,46 +1,46 @@
-const CODES = {
-  A: 65,
-  Z: 90
-}
-
-function creareRow() {
-  return `
-      <div class="row">
-       <div class="row-info"></div>
-       <div class="row-data"></div>
-      </div>
-  `
-}
-
-function createCol(content) {
-  return `<div class="column"> {content} </div>`
-}
-
-function createCell() {
-  return `
-      <div class="cell" contenteditable="true">
-       B2
-      </div>
-  `
-}
-
-function toChar() {
-
-}
-
-function toColumn() {
-
-}
-
-export function createTable(rowsCount = 15) {
-  const colsCount = CODES.Z - CODES.A
-  const rows = []
-
-  const rows = new Array(colsCount)
-    .fill('')
-    .map(el, index => {
-      return
-    })
-
-  return rows.join('')
-}
+// const CODES = {
+//   A: 65,
+//   Z: 90
+// }
+//
+// function createRow() {
+//   return `
+//       <div class="row">
+//        <div class="row-info"></div>
+//        <div class="row-data"></div>
+//       </div>
+//   `
+// }
+//
+// function createCol(content) {
+//   return `<div class="column"> {content} </div>`
+// }
+//
+// function createCell() {
+//   return `
+//       <div class="cell" contenteditable="true">
+//        B2
+//       </div>
+//   `
+// }
+//
+// function toChar() {
+//
+// }
+//
+// function toColumn() {
+//
+// }
+//
+// export function createTable(rowsCount = 15) {
+//   const colsCount = CODES.Z - CODES.A
+//   // const rows = []
+//
+//   const rows = new Array(colsCount)
+//     .fill('')
+//     .map(el, index => {
+//       return
+//     })
+//
+//   return rows.join('')
+// }
